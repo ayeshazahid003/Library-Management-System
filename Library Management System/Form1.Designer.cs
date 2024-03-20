4662060fbebfc711843810db3787716c597ed265
ayeshazahid003 <145323053+ayeshazahid003@users.noreply.github.com>
2024-03-20 08:52:44 +0500

    Add files via upload

---
+++ Library Management System/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Library_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.login = new System.Windows.Forms.Button();
+            this.signUp = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtusername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtusername.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(132, 172);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(158, 20);
+            this.txtusername.TabIndex = 1;
+            this.txtusername.Text = "Username";
+            this.txtusername.MouseEnter += new System.EventHandler(this.txtusername_MouseEnter);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(132, 211);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(164, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.MouseEnter += new System.EventHandler(this.txtPassword_MouseEnter);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(132, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 3);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(132, 227);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(168, 3);
+            this.panel2.TabIndex = 4;
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.login.Location = new System.Drawing.Point(157, 270);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 33);
+            this.login.TabIndex = 6;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = false;
+            // 
+            // signUp
+            // 
+            this.signUp.BackColor = System.Drawing.SystemColors.Info;
+            this.signUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.signUp.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signUp.Location = new System.Drawing.Point(157, 325);
+            this.signUp.Name = "signUp";
+            this.signUp.Size = new System.Drawing.Size(100, 33);
+            this.signUp.TabIndex = 7;
+            this.signUp.Text = "Sign Up";
+            this.signUp.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Library_Management_System.Properties.Resources.image1;
+            this.pictureBox3.Location = new System.Drawing.Point(85, 211);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(29, 16);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Library_Management_System.Properties.Resources.image1;
+            this.pictureBox2.Location = new System.Drawing.Point(85, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 16);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Library_Management_System.Properties.Resources.image;
+            this.pictureBox1.Location = new System.Drawing.Point(123, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exitB
+            // 
+            this.exitB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.exitB.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitB.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.exitB.Location = new System.Drawing.Point(317, 13);
+            this.exitB.Name = "exitB";
+            this.exitB.Size = new System.Drawing.Size(35, 23);
+            this.exitB.TabIndex = 9;
+            this.exitB.Text = "X";
+            this.exitB.UseVisualStyleBackColor = false;
+            this.exitB.Click += new System.EventHandler(this.exitB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(364, 421);
+            this.Controls.Add(this.exitB);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.signUp);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button signUp;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button exitB;
+    }
+}
+
---
+++ Library Management System/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Library_Management_System
+{
+    public partial class Form1 : Form
+    {
+        string Connection = "Data Source=DESKTOP-U7DD4IT\\SQLEXPRESS;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void exitB_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtPassword_MouseEnter(object sender, EventArgs e)
+        {
+            if(txtPassword.Text== "Password")
+            {
+                txtPassword.Clear();
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void txtusername_MouseEnter(object sender, EventArgs e)
+        {
+            if(txtusername.Text=="Username")
+            {
+                txtusername.Clear();
+            }
+        }
+    }
+}
